dbf461e7f1c5c348c7b34997dd977fecef3476c2
sungwonlyu <lyusungwon@naver.com>
2018-09-18 18:59:51 +0900

    rn

--- dataloader.py
+++ dataloader.py
@@ -1,150 +1,176 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 from torchvision import datasets, transforms
+from torch.nn.utils.rnn import pad_sequence, pack_sequence
+import pickle
 import os
 import pandas as pd
 import numpy as np
-from cv2 import imread
-from collections import defaultdict
+from PIL import Image
+from imageio import imread
 import json
 from time import time
 import re
+from collections import defaultdict
 
-def train_loader(data, data_directory = '/home/sungwonlyu/data/', batch_size = 128):
-    if data == 'mnist':
+def collate_text(list_inputs):
+    list_inputs.sort(key=lambda x:len(x[1]), reverse = True)
+    images = torch.Tensor()
+    questions = []
+    answers = torch.Tensor().to(torch.long)
+    for i, q, a in list_inputs:
+        images = torch.cat([images, i.unsqueeze(0)], 0)
+        questions.append(q)
+        answers = torch.cat([answers, a], 0)
+    questions_packed = pack_sequence(questions)
+    return images, questions_packed, answers
+
+def train_loader(data, data_directory = '/home/sungwonlyu/data/', batch_size = 128, input_h = 128, input_w = 128):
+    if data == 'clevr':
         train_dataloader = DataLoader(
-            datasets.MNIST(data_directory + data + '/', train=True, download=True, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'svhn':
-        train_dataloader = DataLoader(
-            datasets.SVHN(data_directory + data + '/', train=True, download=True, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'cifar10':
-        train_dataloader = DataLoader(
-            datasets.CIFAR10(data_directory + data + '/', train=True, download=True, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'alphachu':
-        train_dataloader = DataLoader(
-            AlphachuDataset(data_directory + data + '/', train=True, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'clevr':
-        train_dataloader = DataLoader(
-            Clevr(data_directory + data + '/', train=True, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
+            Clevr(data_directory + data + '/', train=True, 
+            transform = transforms.Compose([transforms.Resize((input_h, input_w)),
+                                                transforms.ToTensor()])),
+            batch_size=batch_size, shuffle=True,
+            collate_fn = collate_text)
     return train_dataloader
 
-def test_loader(data, data_directory = '/home/sungwonlyu/data', batch_size = 128):
-    if data == 'mnist':
+def test_loader(data, data_directory = '/home/sungwonlyu/data', batch_size = 128, input_h = 128, input_w = 128):
+    if data == 'clevr':
         test_dataloader = DataLoader(
-            datasets.MNIST(data_directory + data + '/', train=False, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'svhn':
-        test_dataloader = DataLoader(
-            datasets.SVHN(data_directory + data + '/', train=False, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'cifar10':
-        test_dataloader = DataLoader(
-            datasets.CIFAR10(data_directory + data + '/', train=False, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'alphachu':
-        test_dataloader = DataLoader(
-            AlphachuDataset(data_directory + data + '/', train=False, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
-    elif data == 'clevr':
-        test_dataloader = DataLoader(
-            Clevr(data_directory + data + '/', train=False, transform=transforms.ToTensor()),
-            batch_size=batch_size, shuffle=True)
+            Clevr(data_directory + data + '/', train=False, 
+            transform = transforms.Compose([transforms.Resize((input_h, input_w)),
+                                                transforms.ToTensor()])),
+            batch_size=batch_size, shuffle=True,
+            collate_fn = collate_text)
     return test_dataloader
 
 
 class Clevr(Dataset):
     """Clevr dataset."""
     def __init__(self, root_dir, train = True, transform = None):
-        print(1)
-        self.ckpt = time()
         self.root_dir = root_dir
-        self.q_dir = self.root_dir + 'questions/'
-        self.img_dir = self.root_dir + 'images/'
+        # self.mode = 'sample'
         self.mode = 'train' if train else 'val'
         self.transform = transform
-        self.img_dir = self.img_dir + '{}/'.format(self.mode)
-        self.ann_dir = self.q_dir + 'CLEVR_{}_questions.json'.format(self.mode)
-        with open(self.ann_dir) as f:
-            self.q_list = json.load(f)['questions']
-        print(2, time() - self.ckpt)
-        self.ckpt = time()
-        self.make_corpus()
+        self.q_dir = self.root_dir + 'questions/'+ 'CLEVR_{}_questions.json'.format(self.mode)
+        self.img_dir = self.root_dir + 'images/'+ '{}/'.format(self.mode)
+        if self.mode == 'sample':
+            self.img_dir = self.root_dir + 'images/train/'
+        self.sample = False
+        self.load_data()
 
-    def make_corpus(self):
+    def make_data(self):
         q_corpus = set()
         a_corpus = set()
-        qa_data = []
-        for q_obj in self.q_list:
-            img_dir = q_obj['image_filename']
-            q_text = q_obj['question'].lower()
-            q_text = re.sub('\s+', ' ', q_text)
-            q_text_without_question_mark = q_text[:-1]
-            q_words = q_text_without_question_mark.split(' ')
-            q_corpus.update(q_words)
-            a_text = q_obj['answer'].lower()
-            a_text = re.sub('\s+', ' ', a_text)
-            a_corpus.add(a_text)
-            qa_data.append((img_dir, q_words, a_text))
-        self.q_size = len(q_corpus)
-        self.a_size = len(a_corpus)
-        print(3, time() - self.ckpt)
-        self.ckpt = time()
+        # if self.sample:
+        #     modes = ['sample']
+        # else:
+        modes = ['train', 'val', 'sample']
+        q_list = dict()
+        qa_list = defaultdict(list)
+        for mode in modes:
+            # if self.sample:
+            #     img_dir = self.root_dir + 'images/train/'
+            #     ann_dir = self.root_dir + 'questions/CLEVR_{}_questions.json'.format(mode)
+            # else:
+            img_dir = self.root_dir + 'images/{}/'.format(mode)
+            if mode == 'sample':
+                img_dir = self.root_dir + 'images/train/'
+            ann_dir = self.root_dir + 'questions/CLEVR_{}_questions.json'.format(mode)
+            with open(ann_dir) as f:
+                q_list[mode] = json.load(f)['questions']
+            for q_obj in q_list[mode]:
+                img_dir = q_obj['image_filename']
+                q_text = q_obj['question'].lower()
+                q_text = re.sub('\s+', ' ', q_text)
+                q_text_without_question_mark = q_text[:-1]
+                q_words = q_text_without_question_mark.split(' ')
+                q_words.insert(0, 1)
+                q_words.append(2)
+                q_corpus.update(q_words)
+                a_text = q_obj['answer'].lower()
+                a_text = re.sub('\s+', ' ', a_text)
+                a_corpus.add(a_text)
+                qa_list[mode].append((img_dir, q_words, a_text))
+                # q_corpus.update(a_corpus)
 
-        word_to_idx = {"SOS": 1, "EOS": 2}
-        idx_to_word = {2: "SOS", 1: "EOS"}
+        word_to_idx = {"PAD":0, "SOS": 1, "EOS": 2}
+        idx_to_word = {0: "PAD", 1: "SOS", 2: "EOS"}
+        answer_word_to_idx = dict()
+        answer_idx_to_word = dict()
         for idx, word in enumerate(q_corpus, start=3):
             # index starts with 1 because 0 is used as the padded value when batches are
             #  created
             word_to_idx[word] = idx
             idx_to_word[idx] = word
-        print(4, time() - self.ckpt)
-        self.ckpt = time()
 
-        answer_word_to_idx = dict()
-        answer_idx_to_word = dict()
-        for idx, word in enumerate(a_corpus, start=0):
-            # single answer, so no padded values of 0 are created. thus index starts with 0
+        for idx, word in enumerate(a_corpus):
             answer_word_to_idx[word] = idx
             answer_idx_to_word[idx] = word
-        print(5, time() - self.ckpt)
-        self.ckpt = time()
+        #     # single answer, so no padded values of 0 are created. thus index starts with 0
+        data_dict = {'question': {'word_to_idx' : word_to_idx,
+                                    'idx_to_word' : idx_to_word},
+                        'answer': {'word_to_idx' : answer_word_to_idx,
+                                    'idx_to_word' : answer_idx_to_word}}
+        with open('data_dict.pkl', 'wb') as file:
+            pickle.dump(data_dict, file, protocol=pickle.HIGHEST_PROTOCOL)
+        print('data_dict.pkl saved')
 
-        self.qa_idx_data = []
-        for img_dir, q_word_list, answer_word in qa_data:
-            q = [word_to_idx[word] for word in q_word_list]
-            q.insert(0, 1)
-            q.append(2)
-            a = answer_word_to_idx[answer_word]
-            self.qa_idx_data.append((img_dir, q, a))
-        print(6, time() - self.ckpt)
-        self.ckpt = time()
+        qa_idx_data = defaultdict(list)
+        for mode in modes:
+            for img_dir, q_word_list, answer_word in qa_list[mode]:                  
+                q = [word_to_idx[word] for word in q_word_list]
+                q = torch.from_numpy(np.array(q))
+                a = answer_word_to_idx[answer_word]
+                a = torch.from_numpy(np.array(a)).view(1)
+                qa_idx_data[mode].append((img_dir, q, a))
+            with open('qa_idx_data_{}.pkl'.format(mode), 'wb') as file:
+                pickle.dump(qa_idx_data[mode], file, protocol=pickle.HIGHEST_PROTOCOL)
+            print('qa_idx_data_{}.pkl saved'.format(mode))
 
+    def load_data(self):
+        with open('qa_idx_data_{}.pkl'.format(self.mode), 'rb') as file:
+            self.qa_idx_data = pickle.load(file)
+        with open('data_dict.pkl', 'rb') as file:
+            self.data_dict = pickle.load(file)
+        self.word_to_idx = self.data_dict['question']['word_to_idx']
+        self.idx_to_word = self.data_dict['question']['idx_to_word']
+        self.answer_word_to_idx = self.data_dict['answer']['word_to_idx']
+        self.answer_idx_to_word = self.data_dict['answer']['idx_to_word']
+        self.q_size = len(self.word_to_idx)
+        self.a_size = len(self.answer_word_to_idx)
 
     def __len__(self):
         return len(self.qa_idx_data)
 
     def __getitem__(self, idx):
         img_dir, q, a = self.qa_idx_data[idx]
-        image = imread(self.img_dir + img_dir)
-        image = image[:,:,::-1].transpose((2,0,1)).copy()
-        image = torch.from_numpy(image).float().div(255.0)
-        image = image * 2 - 1
+        image = Image.open(self.img_dir + img_dir).convert('RGB')
         if self.transform:
-            q = self.transform(q)
-            a = self.transform(a)
+            image = self.transform(image)
         return image, q, a
 
 def debug():
-    train_dataloader = train_loader('clevr')
-    for img, q, a in train_dataloader:
-        print(img.size()) 
-        print(q.size()) 
-        print(a.size()) 
-
-
-debug()
+    start = time()
+    train_dataloader = train_loader('clevr', batch_size = 4)
+    b = time() - start
+    print(b)
+    start = time()
+    sum_ = 0
+    n = 0
+    for i, q, a in train_dataloader:
+        n += 1
+        # print(i)
+        # print(q) 
+        # print(a)
+        b = time() - start
+        print(b)
+        sum_ += b   
+        start = time()
+    print(sum_)
+def make_data():
+    train_dataloader = train_loader('clevr', batch_size = 4)
+    train_dataloader.dataset.make_data()
+# make_data()
+# debug()
---
+++ main.py
@@ -0,0 +1,198 @@
+import sys
+sys.path.append('../utils/')
+import argparser
+import dataloader
+import model
+import time
+import os
+import cv2
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.nn.utils.rnn import PackedSequence, pad_packed_sequence
+from tensorboardX import SummaryWriter
+from model import MLP, Conv, Text_encoder
+import numpy as np
+import pickle
+import cProfile
+
+parser = argparser.default_parser()
+# Input
+parser.add_argument('--name', type=str, default='rn')
+parser.add_argument('--channel-size', type=int, default=3)
+parser.add_argument('--input-h', type=list, default=128)
+parser.add_argument('--input-w', type=list, default=128)
+# Convolution
+parser.add_argument('--cv-filter', type=int, default=24)
+
+parser.add_argument('--cv-kernel', type=int, default=3)
+parser.add_argument('--cv-stride', type=int, default=2)
+parser.add_argument('--cv-layer', type=int, default=4)
+# Text Encoder
+parser.add_argument('--te-embedding', type=int, default=32)
+parser.add_argument('--te-hidden', type=int, default=128)
+parser.add_argument('--te-layer', type=int, default=1)
+# g theta
+parser.add_argument('--gt-hidden', type=int, default=256)
+parser.add_argument('--gt-layer', type=int, default=4)
+# f phi
+parser.add_argument('--fp-hidden', type=int, default=256)
+parser.add_argument('--fp-dropout', type=int, default=2)
+parser.add_argument('--fp-dropout_rate', type=float, default=0.5)
+parser.add_argument('--fp-layer', type=int, default=3)
+
+args = parser.parse_args()
+
+torch.manual_seed(args.seed)
+
+if args.device == 'cpu':
+    device = torch.device('cpu')
+else:
+    device = torch.device('cuda:{}'.format(args.device))
+    torch.cuda.set_device(args.device)
+
+config_list = [args.epochs, args.batch_size, args.lr, args.device,
+               'inp', args.channel_size, args.input_h, args.input_w,
+               'cv', args.cv_filter, args.cv_kernel, args.cv_stride, args.cv_layer,
+               'te', args.te_embedding, args.te_hidden, args.te_layer,
+               'gt', args.gt_hidden, args.gt_layer,
+               'fp', args.fp_hidden, args.fp_dropout, args.fp_dropout_rate, args.fp_layer]
+config = '_'.join(map(str, config_list))
+print("Config:", config)
+
+train_loader = dataloader.train_loader('clevr', args.data_directory, args.batch_size, args.input_h, args.input_w)
+test_loader = dataloader.test_loader('clevr', args.data_directory, args.batch_size, args.input_h, args.input_w)
+
+cv_layout = [(args.cv_filter, args.cv_kernel, args.cv_stride) for i in range(args.cv_layer)]
+gt_layout = [args.gt_hidden for i in range(args.gt_layer)]
+gt_layout.insert(0, (args.cv_filter + 1) * 2 + args.te_hidden)
+fp_layout = [args.fp_hidden for i in range(args.fp_layer)]
+fp_layout.append(train_loader.dataset.a_size)
+
+conv = Conv(cv_layout, args.channel_size).to(device)
+text_encoder = Text_encoder(train_loader.dataset.q_size, args.te_embedding, args.te_hidden, args.te_layer).to(device)
+g_theta = MLP(gt_layout).to(device)
+f_phi = MLP(fp_layout, args.fp_dropout, args.fp_dropout_rate, last = True).to(device)
+with open('data_dict.pkl', 'rb') as file:
+    data_dict = pickle.load(file)
+
+if args.load_model != '000000000000':
+    conv.load_state_dict(torch.load(args.log_directory + args.name + '/' + args.load_model + '/conv.pt'))
+    text_encoder.load_state_dict(torch.load(args.log_directory + args.name + '/' + args.load_model + '/text_encoder.pt'))
+    gt.load_state_dict(torch.load(args.log_directory + args.name + '/' + args.load_model + '/gt.pt'))
+    fp.load_state_dict(torch.load(args.log_directory + args.name + '/' + args.load_model + '/fp.pt'))
+    args.time_stamep = args.load_mode[:12]
+
+log = args.log_directory + args.name + '/' + args.time_stamp + config + '/'
+writer = SummaryWriter(log)
+
+optimizer = optim.Adam(text_encoder.parameters(), lr=args.lr)
+
+def object_pair(images, questions):
+    n, c, h, w = images.size()
+    o = h * w
+    hd = questions.size(1)
+    cordinate = torch.linspace(-1, 1, o).view(1, o, 1).expand(n, o, 1).to(device)
+    images = images.view(n, c, o).transpose(1, 2)
+    images = torch.cat([images, cordinate], 2)
+    images1 = images.unsqueeze(1).expand(n, o, o, c + 1).contiguous().view(n, o**2, c + 1)
+    images2 = images.unsqueeze(2).expand(n, o, o, c + 1).contiguous().view(n, o**2, c + 1)
+    questions = questions.unsqueeze(1).expand(n, o**2, hd)
+    pairs = torch.cat([images1, images2, questions], 2)
+    # pairs = pairs.view(n * (o**2), -1)
+    return pairs
+
+def train(epoch):
+    epoch_start_time = time.time()
+    start_time = time.time()
+    train_loss = 0
+    g_theta.train()
+    f_phi.train()
+    conv.train()
+    text_encoder.train()
+    for batch_idx, (image, question, answer) in enumerate(train_loader):
+        a = time.time() - start_time
+        print("load", a)
+        start_time = time.time()
+
+        batch_size = image.size()[0]
+        optimizer.zero_grad()
+        image = image.to(device)
+        question= PackedSequence(question.data.to(device), question.batch_sizes)
+        answer = answer.to(device)
+        objects = conv(image)
+        questions = text_encoder(question)
+        pairs = object_pair(objects, questions)
+        relations = g_theta(pairs)
+        relations_sum = relations.sum(1)
+        output = f_phi(relations_sum)
+        loss = F.cross_entropy(output, answer)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+
+        a = time.time() - start_time
+        print('cal', a)
+        start_time = time.time()
+        if a > 1:
+            print("long")
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tTime: {:.6f}'.format(
+                epoch, batch_idx * batch_size, len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item(), time.time() - start_time))
+
+    print('====> Epoch: {} Average loss: {:.4f}\tTime: {:.4f}'.format(
+        epoch, train_loss / len(train_loader.dataset), time.time() - epoch_start_time))
+    writer.add_scalar('Train loss',  train_loss / len(train_loader.dataset), epoch)
+
+
+def test(epoch):
+    g_theta.eval()
+    f_phi.eval()
+    conv.eval()
+    text_encoder.eval()
+    test_loss = 0
+    for batch_idx, (image, question, answer) in enumerate(train_loader):
+        start_time = time.time()
+        batch_size = image.size()[0]
+        optimizer.zero_grad()
+        image = image.to(device)
+        question= PackedSequence(question.data.to(device), question.batch_sizes)
+        answer = answer.to(device)
+        objects = conv(image)
+        questions = text_encoder(question)
+        pairs = object_pair(objects, questions)
+        relations = g_theta(pairs)
+        relations_sum = relations.view(batch_size, -1, args.gt_hidden).sum(1)
+        output = f_phi(relations_sum)
+        loss = F.cross_entropy(output, answer)
+        test_loss += loss.item()
+        if batch_idx == 0:
+            # n = min(batch_size, 8)
+            n = 1
+            # label = np.ones_like(image[:n, :64])
+            pad_question, lengths = pad_packed_sequence(question)
+            pad_question = pad_question.transpose(0,1)
+            question_text = [' '.join([data_dict['question']['idx_to_word'][i] for i in q]) for q in pad_question.cpu().numpy()[:n]]
+            answer_text = [data_dict['answer']['idx_to_word'][a] for a in answer.cpu().numpy()[:n]]
+            text = 'Quesetion: ' + question_text[0] + '/' + 'Answer: ' + answer_text[0]
+            # for n, (q, a) in enumerate(zip(question_text, answer_text)):       
+            #     text += str(n) + '\n' + 'Quesetion: ' + q + '\n' + 'Answer: ' + a + '\n'
+            print(text)                
+            #     label[i] = cv2.putText(label[i],question_text[i],(2,16), cv2.FONT_HERSHEY_SIMPLEX, 1/5,(0,0,0),1,cv2.LINE_AA)
+            #     label[i] = cv2.putText(label[i],answer_text[i],(2,48), cv2.FONT_HERSHEY_SIMPLEX, 1/5,(0,0,0),1,cv2.LINE_AA)
+            # label = torch.tensor(label).to(device)
+            writer.add_image('Image', torch.cat([image[:n]]), epoch)
+            writer.add_text('QA', '\n'.join(text), epoch)
+    print('====> Test set loss: {:.4f}'.format(test_loss / len(test_loader.dataset)))
+    writer.add_scalar('Test loss',  test_loss / len(test_loader.dataset), epoch)
+
+for epoch in range(args.epochs):
+    train(epoch)
+    test(epoch)
+    torch.save(g_theta, log + 'g_theta.pt')
+    torch.save(f_phi, log + 'f_phi.pt')
+    torch.save(conv, log + 'conv.pt')
+    torch.save(text_encoder, log + 'text_encoder.pt')
+    print('Model saved in ', log)
+writer.close()
