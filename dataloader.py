2c7cf6683f2e26249837bf193807996eab30b3c3
sungwonlyu <lyusungwon@naver.com>
2019-01-30 20:12:27 +0900

    remake

--- dataloader.py
+++ dataloader.py
@@ -9,33 +9,29 @@ from pathlib import Path
 from data_maker import make_questions, make_images
 from utils import is_file_exist
 import h5py
-import numpy as np
 
 home = str(Path.home())
 
 
 def collate_text(list_inputs):
     list_inputs.sort(key=lambda x: len(x[1]), reverse=True)
-    images1 = []
-    images2 = []
+    images = []
     questions = []
     q_length = []
     answers = []
     question_types = []
-    for i1, i2, q, a, types in list_inputs:
-        images1.append(i1)
-        images2.append(i2)
+    for i, q, a, types in list_inputs:
+        images.append(i)
         questions.append(q)
         q_length.append(len(q))
         answers.append(a)
         question_types.append(types)
-    images1 = torch.cat(images1, 0)
-    # images2 = torch.cat(images2, 0)
+    images = torch.cat(images, 0)
     padded_questions = pad_sequence(questions, batch_first=True)
     q_length = torch.Tensor(q_length).to(torch.long)
     answers = torch.cat(answers, 0)
     question_types = torch.cat(question_types, 0)
-    return images1, images2, (padded_questions, q_length), answers, question_types
+    return images, (padded_questions, q_length), answers, question_types
 
 
 def load_dataloader(data_directory, dataset, is_train=True, batch_size=128, data_config=[224, 224, 0, True, 0]):
@@ -60,17 +56,18 @@ class VQA(Dataset):
         self.cv_pretrained = cv_pretrained
         self.transform = transform
         self.question_file = os.path.join(data_dir, dataset, f'questions_{self.mode}_{top_k}.h5')
-        # if self.cv_pretrained:
-        self.image_dir1 = os.path.join(data_dir, dataset, f'images_{self.mode}_{str(size[0])}.h5')
-        self.idx_dict_file = os.path.join(data_dir, dataset, 'idx_dict.pkl')
-        if dataset == 'clevr' or dataset == 'sample':
-            self.image_dir2 = os.path.join(data_dir, dataset, 'images', f'{self.mode}')
-        elif dataset == 'vqa2':
-            self.image_dir2 = os.path.join(data_dir, dataset, f'{self.mode}2014')
+        if self.cv_pretrained:
+            self.image_dir = os.path.join(data_dir, dataset, f'images_{self.mode}_{str(size[0])}.h5')
+            self.idx_dict_file = os.path.join(data_dir, dataset, 'idx_dict.pkl')
+        else:
+            if dataset == 'clevr' or dataset == 'sample':
+                self.image_dir = os.path.join(data_dir, dataset, 'images', f'{self.mode}')
+            elif dataset == 'vqa2':
+                self.image_dir = os.path.join(data_dir, dataset, f'{self.mode}2014')
         if not is_file_exist(self.question_file):
             make_questions(data_dir, dataset, top_k)
         if cv_pretrained:
-            if not is_file_exist(self.image_dir1):
+            if not is_file_exist(self.image_dir):
                 make_images(data_dir, dataset, size)
         self.load_data()
 
@@ -79,56 +76,35 @@ class VQA(Dataset):
         # print(f"Start loading {self.data_file}")
         # with open(self.data_file, 'rb') as file:
         #     self.data = pickle.load(file)
-        # if self.cv_pretrained:
-        print(f"Start loading {self.idx_dict_file}")
-        with open(self.idx_dict_file, 'rb') as file:
-            self.idx_dict = pickle.load(file)[self.mode]
+        if self.cv_pretrained:
+            print(f"Start loading {self.idx_dict_file}")
+            with open(self.idx_dict_file, 'rb') as file:
+                self.idx_dict = pickle.load(file)[self.mode]
 
     def __len__(self):
         return h5py.File(self.question_file, 'r', swmr=True)['questions'].shape[0]
 
     def __getitem__(self, idx):
         question_file = h5py.File(self.question_file, 'r', swmr=True)
+        # image_file, image_id, a, q_t = self.data[idx]
         q = question_file['questions'][idx]
         a = question_file['answers'][idx]
         q_t = question_file['question_types'][idx]
         ii = question_file['image_ids'][idx]
-        # if self.cv_pretrained:
-        image1 = h5py.File(self.image_dir1, 'r', swmr=True)['images'][self.idx_dict[ii]]
-        image1 = torch.from_numpy(image1).unsqueeze(0)
-        print("image1", self.idx_dict[ii])
-
-        image_file = f'COCO_{self.mode}2014_{str(ii).zfill(12)}.jpg' if 'vqa' in self.dataset else f'CLEVR_{self.mode}_{str(ii).zfill(6)}.png'
-        if self.dataset == 'sample':
-            image_file = f'CLEVR_new_{str(ii).zfill(6)}.png'
-        image2 = Image.open(os.path.join(self.image_dir2, image_file)).convert('RGB')
-        # if self.transform:
-        #     image2 = self.transform(image2).unsqueeze(0)
-        image2 = torch.from_numpy(np.array(image2)).to(torch.float)
-        print("image2", ii)
+        if self.cv_pretrained:
+            image = h5py.File(self.image_dir, 'r', swmr=True)['images'][self.idx_dict[ii]]
+            image = torch.from_numpy(image).unsqueeze(0)
+        else:
+            image_file = f'COCO_{self.mode}2014_{str(ii).zfill(12)}.jpg' if 'vqa' in self.dataset else f'CLEVR_{self.mode}_{str(ii).zfill(6)}.png'
+            if self.dataset == 'sample':
+                image_file = f'CLEVR_new_{str(ii).zfill(6)}.png'
+            image = Image.open(os.path.join(self.image_dir, image_file)).convert('RGB')
+            if self.transform:
+                image = self.transform(image).unsqueeze(0)
         q = torch.from_numpy(q).to(torch.long)
         a = torch.Tensor([a]).to(torch.long)
         q_t = torch.Tensor([q_t]).to(torch.long)
-        return image1, image2, q, a, q_t
-        # question_file = h5py.File(self.question_file, 'r', swmr=True)
-        # q = question_file['questions'][idx]
-        # a = question_file['answers'][idx]
-        # q_t = question_file['question_types'][idx]
-        # ii = question_file['image_ids'][idx]
-        # if self.cv_pretrained:
-        #     image = h5py.File(self.image_dir, 'r', swmr=True)['images'][self.idx_dict[ii]]
-        #     image = torch.from_numpy(image).unsqueeze(0)
-        # else:
-        #     image_file = f'COCO_{self.mode}2014_{str(ii).zfill(12)}.jpg' if 'vqa' in self.dataset else f'CLEVR_{self.mode}_{str(ii).zfill(6)}.png'
-        #     if self.dataset == 'sample':
-        #         image_file = f'CLEVR_new_{str(ii).zfill(6)}.png'
-        #     image = Image.open(os.path.join(self.image_dir, image_file)).convert('RGB')
-        #     if self.transform:
-        #         image = self.transform(image).unsqueeze(0)
-        # q = torch.from_numpy(q).to(torch.long)
-        # a = torch.Tensor([a]).to(torch.long)
-        # q_t = torch.Tensor([q_t]).to(torch.long)
-        # return image, q, a, q_t
+        return image, q, a, q_t
 
 
 if __name__ =='__main__':
